4b689572673f5637d135ed2999b6cc39885f4fbf
CyanSkeleton <jhaanubhav8305@gmail.com>
2025-04-08 22:57:25 -0700

    UI Colored
    Added colors to the UI

--- src/main/java/com/jha/anubhav/engineeringchallenge2025/Controller.java
+++ src/main/java/com/jha/anubhav/engineeringchallenge2025/Controller.java
@@ -6,9 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 
 import java.util.*;
 
@@ -34,6 +32,15 @@ public class Controller implements Initializable {
     @FXML
     private Button createNewDeadlineButton;
 
+    @FXML
+    private TextField titleField;
+
+    @FXML
+    private TextField dateField;
+
+    @FXML
+    private TextArea descriptionField;
+
     Deadline deadline1 = new Deadline("Project1", "4/10/2025", "testing");
     Deadline deadline2 = new Deadline("Project2", "6/7/2025", "test");
     Deadline deadline3 = new Deadline("Project3", "5/4/2025", "tests");
@@ -48,7 +55,12 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL arg0, ResourceBundle arg1)
     {
-        System.out.println("e");
+        // This doesn't work when I try moving it to the css file
+        //titleField.setStyle("-fx-control-inner-background: #1C444A; -fx-text-fill:  #A9EAEF; -fx-prompt-text-fill: #5CB5CD;");
+        //dateField.setStyle("-fx-control-inner-background: #1C444A; -fx-text-fill:  #A9EAEF; -fx-prompt-text-fill: #5CB5CD;");
+        //descriptionField.setStyle("-fx-control-inner-background: #1C444A;  " +
+        //        " -fx-focus-color: transparent; -fx-border-style: none; -fx-background-radius: 0.0px;");
+
         deadlineDict.put(LocalDate.parse(deadline1.date, standardFormat), deadline1);
         deadlineDict.put(LocalDate.parse(deadline2.date, standardFormat), deadline2);
         deadlineDict.put(LocalDate.parse(deadline3.date, standardFormat), deadline3);
